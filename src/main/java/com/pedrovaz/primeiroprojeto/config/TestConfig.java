d99c6032abb61e875b6917437cc19ca8df660fd7
Pedro Alves <pedrohvs.alves@gmail.com>
2024-02-28 16:42:58 +0000

    Payment, one-to-one association

--- src/main/java/com/pedrovaz/primeiroprojeto/config/TestConfig.java
+++ src/main/java/com/pedrovaz/primeiroprojeto/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.pedrovaz.primeiroprojeto.entities.Category;
 import com.pedrovaz.primeiroprojeto.entities.Order;
 import com.pedrovaz.primeiroprojeto.entities.OrderItem;
+import com.pedrovaz.primeiroprojeto.entities.Payment;
 import com.pedrovaz.primeiroprojeto.entities.Product;
 import com.pedrovaz.primeiroprojeto.entities.User;
 import com.pedrovaz.primeiroprojeto.entities.enums.OrderStatus;
@@ -69,7 +70,7 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT,u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID,u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID ,u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1);
 		
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 	}
