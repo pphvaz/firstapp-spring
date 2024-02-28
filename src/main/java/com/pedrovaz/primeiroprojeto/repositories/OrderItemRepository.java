839fca99d449d5ae8c1e6eef69a8dc8656541f11
Pedro Alves <pedrohvs.alves@gmail.com>
2024-02-28 16:22:57 +0000

    Product-OrderItem one-to-many association

--- src/main/java/com/pedrovaz/primeiroprojeto/repositories/OrderItemRepository.java
+++ src/main/java/com/pedrovaz/primeiroprojeto/repositories/OrderItemRepository.java
@@ -3,6 +3,7 @@ package com.pedrovaz.primeiroprojeto.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pedrovaz.primeiroprojeto.entities.OrderItem;
+import com.pedrovaz.primeiroprojeto.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 }
