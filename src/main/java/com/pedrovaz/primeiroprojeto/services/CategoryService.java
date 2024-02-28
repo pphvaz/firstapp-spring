83c742b4f4a6da776c7bc1621bdfcdf06f60886c
Pedro Alves <pedrohvs.alves@gmail.com>
2024-02-28 09:46:29 +0000

    Category

---
+++ src/main/java/com/pedrovaz/primeiroprojeto/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.pedrovaz.primeiroprojeto.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedrovaz.primeiroprojeto.entities.Category;
+import com.pedrovaz.primeiroprojeto.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
