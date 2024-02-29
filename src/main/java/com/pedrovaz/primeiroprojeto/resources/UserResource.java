0a266bf44fd975ec2b9092d5a99a818c15f0d9a6
Pedro Alves <pedrohvs.alves@gmail.com>
2024-02-29 10:08:23 +0000

    Exception handler - delete

--- src/main/java/com/pedrovaz/primeiroprojeto/resources/UserResource.java
+++ src/main/java/com/pedrovaz/primeiroprojeto/resources/UserResource.java
@@ -45,7 +45,7 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
-	@DeleteMapping(value ="/{id}")
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete (@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
@@ -56,5 +56,4 @@ public class UserResource {
 		obj = service.update(id, obj);
 		return ResponseEntity.ok().body(obj);
 	}
-	
 }
