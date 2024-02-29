554f02955ecfb84ef6fe96d48ab87f5ea6902a69
Pedro Alves <pedrohvs.alves@gmail.com>
2024-02-29 08:57:37 +0000

    Exception handling - findById

--- src/main/java/com/pedrovaz/primeiroprojeto/services/UserService.java
+++ src/main/java/com/pedrovaz/primeiroprojeto/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.pedrovaz.primeiroprojeto.entities.User;
 import com.pedrovaz.primeiroprojeto.repositories.UserRepository;
+import com.pedrovaz.primeiroprojeto.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
